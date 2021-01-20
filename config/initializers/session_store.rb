959fbc38ddb76caea4226b0a8af13ad421d2b82f
usagi3rain <usagi3rain@gmail.com>
2021-01-20 19:11:22 +0900

    sameSite: Noneを追加

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all, SameSite: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_vtubet'
 end
