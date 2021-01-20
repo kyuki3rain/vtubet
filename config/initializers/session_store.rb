ffc317ef62f60d1556b04a947c2344b28da6f0c1
usagi3rain <usagi3rain@gmail.com>
2021-01-20 19:32:09 +0900

    rails_same_site_cookieを追加

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all, same_site: :none
+  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_vtubet'
 end
