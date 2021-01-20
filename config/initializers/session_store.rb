4cf1404e5e1eb8d25c4ceb9097b413e5724fe336
usagi3rain <usagi3rain@gmail.com>
2021-01-20 19:00:12 +0900

    domain追加

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_vtubet'
+  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_vtubet'
 end
-Rails.application.config.api_only = false
