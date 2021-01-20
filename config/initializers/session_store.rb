e4d9b0c2ab30107b3f1d39971f117dc004040490
usagi3rain <usagi3rain@gmail.com>
2021-01-20 20:27:18 +0900

    a

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_vtubet'
+  Rails.application.config.session_store :cookie_store, key: '_vtubet', same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_vtubet'
 end
