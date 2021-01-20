78d2183a8803e1e5460759d364787ebb9ed5c0c9
usagi3rain <usagi3rain@gmail.com>
2021-01-20 18:33:28 +0900

    tld_lengthを2にしてみる

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_vtubet', domain: :all, tld_length: 2
 else
   Rails.application.config.session_store :cookie_store, key: '_vtubet'
 end
