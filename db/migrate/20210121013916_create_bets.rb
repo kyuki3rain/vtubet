beb3529611c48305e32d97c3b34e9ecddcdd9e9e
usagi3rain <usagi3rain@gmail.com>
2021-01-24 04:26:51 +0900

    dbError修正

--- db/migrate/20210121013916_create_bets.rb
+++ db/migrate/20210121013916_create_bets.rb
@@ -1,5 +1,14 @@
 class CreateBets < ActiveRecord::Migration[6.1]
   def change
+    create_table :chances do |t|
+      t.references :participation, null: false, foreign_key: true, index: true
+      t.integer :bet_type
+      t.integer :rate
+      t.integer :status, null: false, default: 0
+
+      t.timestamps
+    end
+
     create_table :bets do |t|
       t.references :user, null: false, foreign_key: true, index: false
       t.integer :point, null: false, default: 0
