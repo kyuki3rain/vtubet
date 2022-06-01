eae9bc418fde4e64dec062c8b49a40ef896e5eca
kyuki3rain <49717102+kyuki3rain@users.noreply.github.com>
2022-06-01 23:04:32 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -34,7 +34,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
           credentials: true
   end
   allow do
-      origins 'https://vtubet-client.vercel.app/'
+      origins 'https://vtubet-client.vercel.app'
 
       resource '*',
           headers: :any,
